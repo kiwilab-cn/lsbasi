96bf72c49db824f87289f457d43168f58eaa2e3b
曾睿 <zery@cengruideMacBook-Pro.local>
2023-06-29 00:17:12 +0800

    update part.1 code.

--- part-1/src/main.rs
+++ part-1/src/main.rs
@@ -1,3 +1,29 @@
+use strum_macros::Display;
+
+#[derive(Display)]
+enum TokenType {
+    INTEGER,
+    PLUS,
+    EOF
+}
+
+struct Token<T> {
+    type: TokenType,
+    value: T,
+}
+
+impl<T> Token<T> {
+    fn str(&self) -> String {
+        let value = repr(self.value);
+        format!("Token({}, {})", self.type, value)
+    }
+
+    fn repr(&self) -> String() {
+        str(&self)
+    }
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
