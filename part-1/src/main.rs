27a5af6904c457ecdab7e4cc7a2b30c569d33b9b
zery <zengrui@diit.cn>
2023-06-29 18:55:20 +0800

    fix part.q code compilebug.

--- part-1/src/main.rs
+++ part-1/src/main.rs
@@ -7,23 +7,47 @@ enum TokenType {
     EOF
 }
 
+type CalcTokenType = TokenType;
+
 struct Token<T> {
-    type: TokenType,
+    genre: CalcTokenType,
     value: T,
 }
 
-impl<T> Token<T> {
-    fn str(&self) -> String {
-        let value = repr(self.value);
-        format!("Token({}, {})", self.type, value)
+impl<T> Token<T> where T: std::fmt::Display {
+    fn display(&self) -> String {
+        format!("Token({}, {})", &self.genre, &self.value)
     }
 
-    fn repr(&self) -> String() {
-        str(&self)
+}
+
+struct Interpreter<'a, T> {
+    text: String,
+    pos: i32,
+    current_token: Option<&'a Token<T>>,
+}
+
+impl<'a, T> Interpreter<'a, T> {
+    fn new(text: String)-> Self {
+       Self {
+            text: text,
+            pos: 0,
+            current_token: None,
+        }
     }
 }
 
 
 fn main() {
-    println!("Hello, world!");
+    let x = Token {genre: TokenType::INTEGER, value: 3};
+    println!("{}, {}", x.genre.to_string(), x.value);
+    println!("{}", x.display());
+
+    let y: Interpreter<'_, u32> = Interpreter::new(String::from("Test ABC"));
+    let z: Interpreter<'_, u32> = Interpreter{
+        text: String::from("Test ABC."),
+        pos: 0,
+        current_token: None,
+    };
+    println!("{}, {}", z.text, y.text);
 }
