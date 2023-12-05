534ac7d283040b66bb1cb047b2c336d4cf7a5830
zery <zerix.os@gmail.com>
2023-12-05 22:34:50 +0800

    format code.

--- part-1/src/main.rs
+++ part-1/src/main.rs
@@ -1,10 +1,14 @@
-use std::{io::{self, Write}, fmt::Display};
+//use std::{io::{self, Write}, fmt::Display};
+use std::{
+    fmt::Display,
+    io::{self, Write},
+};
 
 #[derive(PartialEq)]
 enum TokenType {
     INTEGER,
     PLUS,
-    EOF
+    EOF,
 }
 
 type CalcTokenType = TokenType;
@@ -24,10 +28,9 @@ struct Token {
     value: Option<char>,
 }
 
-
 impl Display for Token {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match &self.value{
+        match &self.value {
             Some(t) => write!(f, "Token({}, {})", &self.genre, t),
             None => write!(f, "Token({}, None)", &self.genre),
         }
@@ -43,16 +46,24 @@ struct Interpreter {
 impl Display for Interpreter {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match &self.current_token {
-            Some(t) => write!(f, "text:{}, post:{}, current_token:{}", &self.text, &self.pos, t),
-            None => write!(f, "text:{}, post:{}, current_token: None", &self.text, &self.pos),
+            Some(t) => write!(
+                f,
+                "text:{}, post:{}, current_token:{}",
+                &self.text, &self.pos, t
+            ),
+            None => write!(
+                f,
+                "text:{}, post:{}, current_token: None",
+                &self.text, &self.pos
+            ),
         }
     }
 }
 
 impl Interpreter {
-    fn new(text: String)-> Interpreter {
-       Interpreter {
-            text: text,
+    fn new(text: String) -> Interpreter {
+        Interpreter {
+            text,
             pos: 0,
             current_token: None,
         }
@@ -62,25 +73,37 @@ impl Interpreter {
         let text = &self.text;
         let pos = self.pos as usize;
 
-        if pos > text.len() -1 {
-            return Token {genre: CalcTokenType::EOF, value: None};
+        if pos > text.len() - 1 {
+            return Token {
+                genre: CalcTokenType::EOF,
+                value: None,
+            };
         }
 
         let current_char = text.chars().nth(pos).unwrap();
 
         if current_char.is_digit(10) {
-            let token = Token { genre: CalcTokenType::INTEGER, value: Some(current_char)};
+            let token = Token {
+                genre: CalcTokenType::INTEGER,
+                value: Some(current_char),
+            };
             self.pos += 1;
             return token;
         }
 
         if current_char == '+' {
-            let token = Token { genre: CalcTokenType::PLUS, value: Some(current_char)};
+            let token = Token {
+                genre: CalcTokenType::PLUS,
+                value: Some(current_char),
+            };
             self.pos += 1;
             return token;
         }
 
-        return Token{genre: CalcTokenType::EOF, value: None};
+        return Token {
+            genre: CalcTokenType::EOF,
+            value: None,
+        };
     }
 
     fn eat(&mut self, token_type: CalcTokenType) -> Result<(), bool> {
@@ -96,13 +119,27 @@ impl Interpreter {
     fn expr(&mut self) -> u32 {
         self.current_token = Some(self.get_next_token());
 
-        let left = &self.current_token.as_ref().unwrap().value.unwrap().to_digit(10).unwrap();
+        let left = &self
+            .current_token
+            .as_ref()
+            .unwrap()
+            .value
+            .unwrap()
+            .to_digit(10)
+            .unwrap();
         let _ = self.eat(CalcTokenType::INTEGER);
 
         let _op = &self.current_token.as_ref().unwrap();
         let _ = self.eat(CalcTokenType::PLUS);
 
-        let right = &self.current_token.as_ref().unwrap().value.unwrap().to_digit(10).unwrap();
+        let right = &self
+            .current_token
+            .as_ref()
+            .unwrap()
+            .value
+            .unwrap()
+            .to_digit(10)
+            .unwrap();
         let _ = self.eat(CalcTokenType::INTEGER);
 
         let result = left + right;
@@ -110,8 +147,7 @@ impl Interpreter {
     }
 }
 
-
-fn main() -> io::Result<()>{
+fn main() -> io::Result<()> {
     loop {
         print!("calc >");
         io::stdout().flush().unwrap();
@@ -126,5 +162,4 @@ fn main() -> io::Result<()>{
         println!("{}", result);
         io::stdout().flush().unwrap();
     }
-
 }
